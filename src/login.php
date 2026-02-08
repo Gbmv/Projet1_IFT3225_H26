7a18444f7f5966ebe12c3ac3725eb70ea036dccc
Gbmv <gabriel.magnoviana@hotmail.com>
2026-02-08 15:12:26 -0500

    Changing the visual of login and register

--- src/login.php
+++ src/login.php
@@ -8,27 +8,50 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Sign In</h1>
-
-    <div class="container">
-        <h1>Register</h1>
-        <!--Formulaire d'inscription-->
-        <form method="POST">
-            <label>Email
-                <input type="email" class="form-control" placeholder="Enter your email"></input>
-            </label>
-            <label>Password
-                <input type="password" class="form-control" placeholder="Enter your password"></input>
-            </label>
-            <button>Connect</button>
-        </form>
-        
-        <a role="button" href="login.php">Create an account</a>
-        <a role="button" href="login.php">Already have an account? Click here to sign in</a>
-
-        <button>Login</button>
-        <a class="" role="button" href="register.php">Don't have an account? Click here to register</a>
+    
+    <!--Logo-->
+    <div class="container mt-5">
+        <div class="d-flex flex-column align-items-center mb-4">
+            <img src="../images/logo.png" alt="Logo" width="300" class="img-fluid mb-2">
+            <h2 class="fw-bold">The Library</h2>
+        </div>
     </div>
+    
+    <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class= "card shadow-sm" style="width: 400px; width: 100%;">
+            <div class="card-body p-4">
+                <h2 class = "text center mb-4">Sing-in</h2>
+    </div>
+    
+    <!--Formulaire de connexion-->            
+    <form method = "POST">
+        <div class = "m-3">
+            <label for = "email" class ="form-label">E-mail</label>
+            <input type="email" class="form-control" placeholder="Enter your email" required>
+        </div>
+
+        <div class = "m-3">
+            <label for = "password" class = "form-label">Password</label>
+            <input type="password" class="form-control" placeholder="Enter your password" required>
+        </div>
+
+        <a class="d-block m-3" href="#">Forgot password?</a>
+
+        <div class= "dgrid btn sm " >
+            <button type="submit" class="btn btn-primary ">Login</button>
+        </div> 
+
+
+        <div class="text center m-3">
+            <a class="mb-3" role="button" href="register.php">Don't have an account? Click here to register</a>
+        </div>
+
+
+    </form>    
+    
+    
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
--- src/register.php
+++ src/register.php
@@ -7,26 +7,49 @@
     <title>Registering space</title>
 </head>
 <body>
-    <div class="container">
-        <h1>Register</h1>
+     <!--Logo-->
+    <div class="container mt-5">
+        <div class="d-flex flex-column align-items-center mb-4">
+            <img src="../images/logo.png" alt="Logo" width="300" class="img-fluid mb-2">
+            <h2 class="fw-bold">The Library</h2>
+        </div>
+    </div>
+    <!--Register box-->
+    <div class="container d-flex align-items-center justify-content-center " style="min-height: 100vh;">
+        <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class= "card shadow-sm" style="width: 100%; width: 100%;">
+            <div class="card-body p-4">
+                <h2 class = "text center mb-4">Register</h2>
+    </div>
         <!--Formulaire d'inscription-->
         <form method="POST" action="data_processing.php">
-            <div class="input-group">
-                <span class="input-group-text">John Doe</span>
-                <input type="text" name="name" class="form-control" placeholder="Enter your name"></input>
-            <div class="input-group">
-                <span class="input-group-text">johndoe@outlook.com</span>
-                <input type="email" name="email" class="form-control" placeholder="Enter your email"></input>
+            <div class = "m-3">
+                <label for = "email" class ="form-label">E-mail</label>
+                <input type="email" class="form-control" placeholder="Enter your email" required>
             </div>
-            <div class="input-group">
-                <span class="input-group-text">johndoe123</span>
-                <input type="password" name="pwd" class="form-control" placeholder="Enter your password"></input>
+
+            <div class = "m-3">
+                <label for = "email" class ="form-label">Confirm your E-mail</label>
+                <input type="email" class="form-control" placeholder="Enter your email" required>
             </div>
-            <!-- Clicking on this button will submit the form -->
-            <button type="submit" name="ok">Create account</button>
-        </form>
+
+            <div class = "m-3">
+                <label for = "password" class = "form-label">Password</label>
+                <input type="password" class="form-control" placeholder="Enter your password" required>
+            </div>
+
+             <!-- Clicking on this button will submit the form -->
+            <div class = "m-3">
+                <button type="submit" name="ok">Create account</button>
+            </div>
+
+             <div class="text center m-3">
+                <a role="button" href="login.php">Already have an account? Click here</a>
+            </div>
         
-        <a role="button" href="login.php">Already have an account? Click here</a>
+            </form>
+        
+
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
